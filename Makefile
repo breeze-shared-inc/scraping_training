8aebc5b72a55ffeee39fd6a7591bd532721cdd52
Black Dragon(白い巨匠) <48268998+bzgwhite@users.noreply.github.com>
2022-04-26 11:27:32 +0900

    pyDocの挿入

--- Makefile
+++ Makefile
@@ -1,17 +1,2 @@
 install:
 	pip3 install -r requirements.txt
-	cd frontend && npm install
-	@make build-front
-
-build-front:
-	cd frontend && npm run build
-	rm -rf backend/dist
-	mv frontend/dist backend/dist
-
-up:
-	python3 application.py
-
-reload:
-	@make build-front
-	@make up
-
--- backend/scrape.py
+++ backend/scrape.py
@@ -1,75 +1,67 @@
 import math
 import time
 import datetime
+import sys
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
 
 
 class Scraper:
+    """
+    記事をスクレイピングして、CSVまたはExcelに吐き出すクラス
 
-    def __init__(self,count,type,all_flg):
+    Attributes
+    ----------
+    ARTICLE_PER_PAGE : int
+        1ページ単位の記事数
+    URL              : str
+        スクレイピング対象のURL
+    JST              : timezone
+        日本時間
+    WAIT_TIME        : int
+        待ち時間
+    all_flg          : bool
+        全件取得フラグ
+    count            : int
+        取得記事数
+    format             : str
+        拡張子(csv or excel)
+    """
+
+    def __init__(self, count: int, format: str, all_flg: bool):
+        """
+        Parameters
+        ----------
+        count   : int
+            記事取得数
+        format  : str
+            拡張子(csv or excel)
+        all_flg : bool
+            全件取得フラグ
+        """
         self.ARTICLE_PER_PAGE = 10
         self.URL = 'https://breezegroup.co.jp'
         self.JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
         self.WAIT_TIME = 5
-        self.all_flg = all_flg
-        self.count = count
-        self.type = type
-        self._finish_count = 0
-        self._max_count = 0
-        self._complete = 0
-        self.observer = []
+        self.all_flg: bool = all_flg
+        self.count: int = count
+        self.format: str = format
 
-    def addObserver(self,observer):
-        self.observer.append(observer)
+    def setURLList(self,count: int):
+        """
+        取得対象のURLリストを取得
 
-    def notice_finish_data(self):
-        for observer in self.observer:
-            observer.send_finish()
+        Parameters
+        ----------
+        count : int
+            取得する記事数
 
-    def notice_max(self):
-        for observer in self.observer:
-            observer.send_max(self._max_count)
-
-    def notice_complete(self):
-        for observer in self.observer:
-            observer.complete()
-
-    @property
-    def finish_data_count(self):
-        return self._finish_count
-
-    @finish_data_count.setter
-    def finish_data_count(self,count):
-        if self._finish_count != count:
-            self._finish_count = count
-            self.notice_finish_data()
-            print(self.finish_data_count)
-
-    @property
-    def max_count(self):
-        return self._max_count
-
-    @max_count.setter
-    def max_count(self,count):
-        if self._max_count != count:
-            self._max_count = count
-            self.notice_max()
-
-    @property
-    def complete(self):
-        return self._complete
-
-    @complete.setter
-    def complete(self,flg):
-        if self._complete != flg:
-            self._complete = flg
-            self.notice_complete()
-
-
-    # 取得対象のURLを取得
-    def setURLList(self,count):
+        Returns
+        -------
+        url_list : list
+            指定した記事数分のURLリスト
+        """
         url_list = []
         page_count = math.ceil(count / self.ARTICLE_PER_PAGE)
         last_page_count = count % self.ARTICLE_PER_PAGE
@@ -90,8 +82,21 @@ class Scraper:
 
         self.max_count = len(url_list)
         return url_list
-    # 記事データを取得
-    def getArticleData(self,url):
+
+    def getArticleData(self,url: str):
+        """
+        記事の詳細データを取得
+
+        Parameters
+        ----------
+        url : str
+            取得して欲しい記事詳細URL
+
+        Returns
+        -------
+        article_data : list
+            取得したデータのリスト。URL、タイトル、いいね数、記事作成日、記事更新日、取得日時
+        """
         article_data = []
         res = requests.get(url)
         soup = bs(res.text,'html.parser')
@@ -108,14 +113,26 @@ class Scraper:
         article_data.append(create_date)
         article_data.append(update_date)
         article_data.append(now.strftime('%Y-%m-%d %H:%M:%S'))
-        self.finish_data_count = self._finish_count + 1
 
         time.sleep(self.WAIT_TIME)
 
         return article_data
 
-    # 対象分だけ繰り返し
-    def getArticleIterator(self,count):
+    def getArticleIterator(self,count: int):
+        """
+        指定した記事数分スクレピングを実施する
+
+        Parameters
+        ----------
+        count : int
+            取得したい記事の数
+
+        Returns
+        -------
+        article_list : list
+            取得した記事情報
+
+        """
         article_list = []
         url_list = self.setURLList(count)
 
@@ -125,7 +142,21 @@ class Scraper:
         return article_list
 
     # 吐き出し用データ整形処理
-    def makeDataFrame(self,article_list):
+    def makeDataFrame(self,article_list: list):
+        """
+        list -> 吐き出し用データ(csv,excel)整形処理
+
+        Parameters
+        ----------
+        article_list : list
+            取得済みの記事リスト
+
+        Returns
+        -------
+        pd : DataFrame
+            データフレーム形式に変換されたデータ
+
+        """
         return pd.DataFrame(
             article_list,
             columns=[
@@ -139,7 +170,16 @@ class Scraper:
             ]
         ).set_index('記事URL')
 
+    # 最大件数の取得
     def maxArticle(self):
+        """
+        最大件数の取得
+
+        Returns
+        -------
+        max_count : int
+            サイト内に存在する記事の数
+        """
         max_count = 0
         max_base_count = 0
         res = requests.get(self.URL)
@@ -153,55 +193,88 @@ class Scraper:
         soup = bs(res.text,'html.parser')
         elems = soup.find_all('a',attrs={'class': 'post-thumbnail'})
         max_count = max_base_count + len(elems)
-        print(max_count)
         time.sleep(5)
 
         return max_count
 
-    def export(self,article_data_frame,file_path = ''):
-        if self.type == 'csv':
-            path = self.csvMaker(article_data_frame,file_path)
-        elif self.type == 'excel':
-            path = self.excelMaker(article_data_frame,file_path)
+    # exportの総合受付
+    def export(self, article_data_frame: pd.DataFrame, file_path = ''):
+        """
+        exportの総合受付
 
-        return path
+        Parameters
+        ----------
+        article_data_frame: Dataframe
+            データフレーム整形後の記事データ
 
-    def csvMaker(self,article_data_frame,file_path = ''):
-        path = file_path
-        article_data_frame.to_csv(path + 'result.csv')
-        return path
+        file_path: str
+            保存先のパス
+        """
+        if self.format == 'csv':
+            self.__csvMaker(article_data_frame,file_path)
+        elif self.format == 'excel':
+            self.__excelMaker(article_data_frame,file_path)
 
 
-    def excelMaker(self,article_data_frame,file_path = ''):
-        path = file_path
-        article_data_frame.to_excel(path + 'result.xlsx')
-        return path
+    def __csvMaker(self, article_data_frame: pd.DataFrame, file_path = ''):
+        """
+        csv吐き出しメソッド
 
-    def facade(self,path = ''):
+        Parameters
+        ----------
+        article_data_frame: Dataframe
+            データフレーム整形後の記事データ
 
-        print(self.all_flg)
-        if self.all_flg:
-            self.count = self.maxArticle()
+        file_path: str
+            保存先のパス
+        """
+        article_data_frame.to_csv(file_path + 'result.csv')
 
-        print(self.count)
-        data = self.getArticleIterator(self.count)
-        data = self.makeDataFrame(data)
-        self.export(data,path)
-        self.complete = 1
+
+    def __excelMaker(self, article_data_frame: pd.DataFrame, file_path = ''):
+        """
+        Excel吐き出しメソッド
+
+        Parameters
+        ----------
+        article_data_frame: Dataframe
+            データフレーム整形後の記事データ
+
+        file_path: str
+            保存先のパス
+        """
+        article_data_frame.to_excel(file_path + 'result.xlsx')
+
 
     @staticmethod
-    def fromWebMaker(number,format,all,ws):
-        from backend.observer import Observer as ob
-        path = './backend/dist/static/'
-        observer = ob(ws)
-        scraper = Scraper(number,format,all)
-        scraper.addObserver(observer)
-        scraper.facade(path)
+    def fromCliMaker(count: int, format='excel', all_flg=False):
+        """
+        記事取得から吐き出しまで一気に実行する関数。
+        利用者は基本的にこのメソッドのみ知っていれば問題なし。
 
-    @staticmethod
-    def fromCliMaker(number,format,all=False):
-        scraper = Scraper(number,format,all)
-        scraper.facade()
+        Parameter
+        ---------
+        count  : int
+            取得したい記事の数
+        format : str
+            吐き出す拡張子。csvまたはExcel。初期値はExcel
+        all_flg: bool
+            全件取得するかどうかのフラグ。
+            Trueにした場合は、引数countは無視され、
+            記事全件取得される
+        """
+        scraper = Scraper(count,format,all_flg)
+        print(scraper.all_flg)
+        if scraper.all_flg:
+            scraper.count = scraper.maxArticle()
+
+        print(scraper.count)
+        data = scraper.getArticleIterator(scraper.count)
+        data = scraper.makeDataFrame(data)
+        scraper.export(data)
 
 if __name__ in '__main__':
-    Scraper.fromCliMaker(5,'csv')
+    args = ['',1,'excel',False]
+    for (arg,i) in zip(sys.argv,range(len(args))):
+        args[i] = arg
+    Scraper.fromCliMaker(int(args[1]),args[2],args[3])
